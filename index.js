3c6d5c40b81c670d4307d58b82c84687f84faa00
Cedric <116574782+TimoTimo78@users.noreply.github.com>
2023-07-25 16:56:02 +0200

    Add Hello 🎉 ExpressWorld 🎉to navigator

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express()
 const PORT = 2023
 
+app.get('/', (req, res) => {
+    res.send('🎉 Hello ExpressWorld! 🎉')
+  })
+
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`Server up and running on port http://localhost:${PORT}`)
   })
