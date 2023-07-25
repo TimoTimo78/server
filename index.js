1ddb52473ed951d766ed3e985c80d226478a4f9f
Cedric <116574782+TimoTimo78@users.noreply.github.com>
2023-07-25 17:12:50 +0200

    Add to index.js "const cors" and "app.use cors"

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const app = express()
-const PORT = 4000
+const PORT = 2023
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('🎉 Hello ExpressWorld! 🎉')
@@ -8,4 +11,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server up and running on port http://localhost:${PORT}`)
-  })
+})
